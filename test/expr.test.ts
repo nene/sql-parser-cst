ef8c070b3895930aedb341e39636de096a7aaa65
Rene Saarsoo <nene@triin.net>
2022-10-09 20:25:31 +0300

    Use external snapshot file

--- test/expr.test.ts
+++ test/expr.test.ts
@@ -7,72 +7,26 @@ describe("expr", () => {
 
   describe("string", () => {
     it("single-quoted string", () => {
-      expect(parseExpr(`'hello'`)).toMatchInlineSnapshot(`
-        {
-          "text": "'hello'",
-          "type": "string",
-        }
-      `);
+      expect(parseExpr(`'hello'`)).toMatchSnapshot();
     });
 
     it("double-quoted string", () => {
-      expect(parseExpr(`"hello"`)).toMatchInlineSnapshot(`
-        {
-          "text": ""hello"",
-          "type": "string",
-        }
-      `);
+      expect(parseExpr(`"hello"`)).toMatchSnapshot();
     });
 
     it("hex literal", () => {
-      expect(parseExpr(`0xAAFF11`)).toMatchInlineSnapshot(`
-        {
-          "prefix": null,
-          "text": "0xAAFF11",
-          "type": "string",
-        }
-      `);
-      expect(parseExpr(`_binary 0xAAFF11`)).toMatchInlineSnapshot(`
-        {
-          "prefix": "_binary",
-          "text": "0xAAFF11",
-          "type": "string",
-        }
-      `);
+      expect(parseExpr(`0xAAFF11`)).toMatchSnapshot();
+      expect(parseExpr(`_binary 0xAAFF11`)).toMatchSnapshot();
     });
 
     it("bit string", () => {
-      expect(parseExpr(`b'011001'`)).toMatchInlineSnapshot(`
-        {
-          "prefix": null,
-          "text": "b'011001'",
-          "type": "string",
-        }
-      `);
-      expect(parseExpr(`_binary b'011001'`)).toMatchInlineSnapshot(`
-        {
-          "prefix": "_binary",
-          "text": "b'011001'",
-          "type": "string",
-        }
-      `);
+      expect(parseExpr(`b'011001'`)).toMatchSnapshot();
+      expect(parseExpr(`_binary b'011001'`)).toMatchSnapshot();
     });
 
     it("hex string", () => {
-      expect(parseExpr(`x'AFC123'`)).toMatchInlineSnapshot(`
-        {
-          "prefix": null,
-          "text": "x'AFC123'",
-          "type": "string",
-        }
-      `);
-      expect(parseExpr(`_binary x'AFC123'`)).toMatchInlineSnapshot(`
-        {
-          "prefix": "_binary",
-          "text": "x'AFC123'",
-          "type": "string",
-        }
-      `);
+      expect(parseExpr(`x'AFC123'`)).toMatchSnapshot();
+      expect(parseExpr(`_binary x'AFC123'`)).toMatchSnapshot();
     });
   });
 });
