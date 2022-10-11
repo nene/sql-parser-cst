8a7f05f2b8422a9a795f7ab63dd14b1d4014739b
Rene Saarsoo <nene@triin.net>
2022-10-11 10:49:07 +0300

    Simpler grouping of operator tests

--- test/expr.test.ts
+++ test/expr.test.ts
@@ -30,61 +30,39 @@ describe("expr", () => {
     ].forEach((op) => {
       it(`parses ${op} operator`, () => {
         testExpr(`5 ${op} 7`);
+        testExpr(`6 /* com1 */ ${op} /* com2 */ 7`);
       });
     });
 
-    it("parses chain of addition and subtraction", () => {
+    it("parses chain of operators", () => {
       testExpr(`5 + 6 - 8`);
-    });
-
-    it("parses chain of multiplication and division", () => {
       testExpr(`2 * 7 / 3`);
-    });
-
-    it("treats multiplication with higher precedence than addition", () => {
-      testExpr(`6 + 7 * 3`);
+      testExpr(`6 + 7 * 3 - 16 / 2`);
     });
 
     it("parses DIV operator", () => {
       testExpr(`8 DIV 4`);
-    });
-
-    it("recognizes lowercase DIV operator", () => {
       testExpr(`8 div 4`);
-    });
-
-    it("parses addition with comments", () => {
-      testExpr(`6 /* com1 */ + /* com2 */ 7`);
-    });
-
-    it("parses multiplication with comments", () => {
-      testExpr(`6 /* com1 */ * /* com2 */ 7`);
-    });
-
-    it("parses comparison with comments", () => {
-      testExpr(`6 /* com1 */ < /* com2 */ 7`);
+      testExpr(`8 /* com1 */ DIV /* com2 */ 2`);
     });
 
     it("parses IS operator", () => {
       testExpr(`7 IS 5`);
-    });
-    it("parses IS operator with comments", () => {
       testExpr(`7 /*c1*/ IS /*c2*/ 5`);
     });
 
     it("parses IS NOT operator", () => {
       testExpr(`7 IS NOT 5`);
-    });
-    it("parses IS NOT operator with comments", () => {
       testExpr(`7 /*c1*/ IS /*c2*/ NOT /*c3*/ 5`);
     });
 
-    it("parses LIKE & NOT LIKE operators", () => {
+    it("parses LIKE operator", () => {
       testExpr(`'foobar' LIKE 'foo%'`);
-      testExpr(`'foobar' NOT LIKE 'foo%'`);
-    });
-    it("parses LIKE & NOT LIKE operators with comments", () => {
       testExpr(`'foobar' /*c1*/ LIKE /*c2*/ 'foo%'`);
+    });
+
+    it("parses NOT LIKE operator", () => {
+      testExpr(`'foobar' NOT LIKE 'foo%'`);
       testExpr(`'foobar' /*c1*/ NOT /*c2*/ LIKE /*c3*/ 'foo%'`);
     });
   });
