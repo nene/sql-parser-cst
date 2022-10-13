7f53af4ced7103225533d400ccc22915da1d61cf
Rene Saarsoo <nene@triin.net>
2022-10-13 13:27:59 +0300

    Bake showAll() functionality inside the main show() function

--- src/show.ts
+++ src/show.ts
@@ -30,12 +30,20 @@ import {
 } from "pegjs/mysql";
 import { isDefined } from "./util";
 
-export function show(node: Node | Node[] | string): string {
+type NodeArray = (Node | NodeArray | string | undefined)[];
+
+export function show(
+  node: Node | NodeArray | string,
+  joinString: string = " "
+): string {
   if (typeof node === "string") {
     return node;
   }
   if (node instanceof Array) {
-    return node.map(show).join(" ");
+    return node
+      .filter(isDefined)
+      .map((n) => show(n))
+      .join(joinString);
   }
 
   return [
@@ -104,11 +112,6 @@ function showNode(node: Node): string {
   }
 }
 
-const showAll = (
-  nodes: (Node | Node[] | string | undefined)[],
-  joinString: string = " "
-) => nodes.filter(isDefined).map(show).join(joinString);
-
 const showComments = (c?: Comment[]): string | undefined => {
   if (!c) {
     return undefined;
@@ -120,7 +123,7 @@ const showComment = (c: Comment): string =>
   c.type === "line_comment" ? c.text + "\n" : c.text;
 
 const showSelectStatement = (node: SelectStatement) =>
-  showAll([
+  show([
     node.select,
     node.from,
     node.where,
@@ -130,7 +133,7 @@ const showSelectStatement = (node: SelectStatement) =>
   ]);
 
 const showSelectClause = (node: SelectClause) =>
-  show(node.selectKw) + " " + showAll(node.columns, ", ");
+  show(node.selectKw) + " " + show(node.columns, ", ");
 
 const showFromClause = (node: FromClause) => {
   // first one is always a table reference expression, the rest are joins
@@ -149,61 +152,59 @@ const showFromClause = (node: FromClause) => {
 };
 
 const showJoin = (node: Join) =>
-  showAll([node.operator, node.table, node.specification]);
+  show([node.operator, node.table, node.specification]);
 
 const showJoinSpecification = (node: JoinSpecification) =>
-  showAll([node.kw, node.expr]);
+  show([node.kw, node.expr]);
 
-const showWhereClause = (node: WhereClause) =>
-  showAll([node.whereKw, node.expr]);
+const showWhereClause = (node: WhereClause) => show([node.whereKw, node.expr]);
 
 const showGroupByClause = (node: GroupByClause) =>
-  show(node.groupByKw) + " " + showAll(node.columns, ", ");
+  show(node.groupByKw) + " " + show(node.columns, ", ");
 
 const showHavingClause = (node: HavingClause) =>
-  showAll([node.havingKw, node.expr]);
+  show([node.havingKw, node.expr]);
 
 const showOrderByClause = (node: OrderByClause) =>
-  show(node.orderByKw) + " " + showAll(node.specifications, ", ");
+  show(node.orderByKw) + " " + show(node.specifications, ", ");
 
 const showSortSpecification = (node: SortSpecification) =>
-  showAll([node.expr, node.orderKw]);
+  show([node.expr, node.orderKw]);
 
-const showAlias = (node: Alias) => showAll([node.expr, node.asKw, node.alias]);
+const showAlias = (node: Alias) => show([node.expr, node.asKw, node.alias]);
 
 const showLiteral = (
   node: StringLiteral | NumberLiteral | BoolLiteral | NullLiteral
 ) => node.text;
 
 const showDateTimeLiteral = (node: DateTimeLiteral) =>
-  showAll([node.kw, node.string]);
+  show([node.kw, node.string]);
 
-const showExprList = (node: ExprList) => showAll(node.children, ", ");
+const showExprList = (node: ExprList) => show(node.children, ", ");
 
 const showParenExpr = (node: ParenExpr) => "(" + show(node.expr) + ")";
 
 const showBinaryExpr = (node: BinaryExpr) =>
-  showAll([node.left, node.operator, node.right]);
+  show([node.left, node.operator, node.right]);
 
 const showUnaryExpr = (node: UnaryExpr) => {
   if (typeof node.operator === "string") {
-    return showAll([node.operator, node.expr], "");
+    return show([node.operator, node.expr], "");
   } else {
-    return showAll([node.operator, node.expr], " ");
+    return show([node.operator, node.expr], " ");
   }
 };
 
 const showBetweenExpr = (node: BetweenExpr) =>
-  showAll([node.left, node.betweenKw, node.begin, node.andKw, node.end]);
+  show([node.left, node.betweenKw, node.begin, node.andKw, node.end]);
 
 const showKeyword = (kw: Keyword) => kw.text;
 
 const showStringWithCharset = (node: StringWithCharset) =>
   node.charset + " " + show(node.string);
 
-const showColumnRef = (node: ColumnRef) =>
-  showAll([node.table, node.column], ".");
+const showColumnRef = (node: ColumnRef) => show([node.table, node.column], ".");
 
-const showTableRef = (node: TableRef) => showAll([node.db, node.table], ".");
+const showTableRef = (node: TableRef) => show([node.db, node.table], ".");
 
 const showIdentifier = (node: Identifier) => node.text;
