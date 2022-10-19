3d9caae95080f8659ccedca37cd52b737dbc8352
Rene Saarsoo <nene@triin.net>
2022-10-19 10:58:27 +0300

    Add tests for whitespace-preserving parser options

--- test/whitespace.test.ts
+++ test/whitespace.test.ts
@@ -1,6 +1,181 @@
-import { parse, show, test } from "./test_utils";
+import { parse, parseExpr, show, test } from "./test_utils";
 
 describe("whitespace", () => {
+  it("by default does not include any whitespace to syntax tree", () => {
+    expect(parseExpr("1 /*com*/ + \n\n 2")).toMatchInlineSnapshot(`
+      {
+        "left": {
+          "text": "1",
+          "type": "number",
+        },
+        "operator": "+",
+        "right": {
+          "text": "2",
+          "type": "number",
+        },
+        "type": "binary_expr",
+      }
+    `);
+  });
+
+  it("preserveNewline:true adds line feeds to syntax tree", () => {
+    expect(parseExpr("1 /*com*/ + \n\n 2", { preserveNewlines: true })).toMatchInlineSnapshot(`
+      {
+        "left": {
+          "text": "1",
+          "type": "number",
+        },
+        "operator": "+",
+        "right": {
+          "leading": [
+            {
+              "text": "
+      ",
+              "type": "newline",
+            },
+            {
+              "text": "
+      ",
+              "type": "newline",
+            },
+          ],
+          "text": "2",
+          "type": "number",
+        },
+        "type": "binary_expr",
+      }
+    `);
+  });
+
+  it("preserveSpaces:true adds horizontal spaces to syntax tree", () => {
+    expect(parseExpr("1 /*com*/ + \n\n 2", { preserveSpaces: true })).toMatchInlineSnapshot(`
+      {
+        "leading": [
+          {
+            "text": " ",
+            "type": "space",
+          },
+        ],
+        "left": {
+          "text": "1",
+          "trailing": [
+            {
+              "text": " ",
+              "type": "space",
+            },
+            {
+              "text": " ",
+              "type": "space",
+            },
+          ],
+          "type": "number",
+        },
+        "operator": "+",
+        "right": {
+          "leading": [
+            {
+              "text": " ",
+              "type": "space",
+            },
+            {
+              "text": " ",
+              "type": "space",
+            },
+          ],
+          "text": "2",
+          "type": "number",
+        },
+        "type": "binary_expr",
+      }
+    `);
+  });
+
+  it("preserveComments:true adds comments to syntax tree", () => {
+    expect(parseExpr("1 /*com*/ + \n\n 2", { preserveComments: true })).toMatchInlineSnapshot(`
+      {
+        "left": {
+          "text": "1",
+          "trailing": [
+            {
+              "text": "/*com*/",
+              "type": "block_comment",
+            },
+          ],
+          "type": "number",
+        },
+        "operator": "+",
+        "right": {
+          "text": "2",
+          "type": "number",
+        },
+        "type": "binary_expr",
+      }
+    `);
+  });
+
+  it("preserveComments/preserveSpaces/preserveNewlines combined include all whitespace to syntax tree", () => {
+    expect(
+      parseExpr("1 /*com*/ + \n\n 2", {
+        preserveComments: true,
+        preserveSpaces: true,
+        preserveNewlines: true,
+      })
+    ).toMatchInlineSnapshot(`
+      {
+        "leading": [
+          {
+            "text": " ",
+            "type": "space",
+          },
+        ],
+        "left": {
+          "text": "1",
+          "trailing": [
+            {
+              "text": " ",
+              "type": "space",
+            },
+            {
+              "text": "/*com*/",
+              "type": "block_comment",
+            },
+            {
+              "text": " ",
+              "type": "space",
+            },
+          ],
+          "type": "number",
+        },
+        "operator": "+",
+        "right": {
+          "leading": [
+            {
+              "text": " ",
+              "type": "space",
+            },
+            {
+              "text": "
+      ",
+              "type": "newline",
+            },
+            {
+              "text": "
+      ",
+              "type": "newline",
+            },
+            {
+              "text": " ",
+              "type": "space",
+            },
+          ],
+          "text": "2",
+          "type": "number",
+        },
+        "type": "binary_expr",
+      }
+    `);
+  });
+
   describe("comments", () => {
     it("parses line comments", () => {
       test("SELECT 1 + -- comment 1\n -- comment 2\n 2");
