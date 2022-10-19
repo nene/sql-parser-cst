1c79df5c28fa529c0bb6235917101f62a8b618da
Rene Saarsoo <nene@triin.net>
2022-10-19 10:46:35 +0300

    Rename comments test to whitespace test

---
+++ test/whitespace.test.ts
@@ -0,0 +1,28 @@
+import { parse, show, test } from "./test_utils";
+
+describe("whitespace", () => {
+  describe("comments", () => {
+    it("parses line comments", () => {
+      test("SELECT 1 + -- comment 1\n -- comment 2\n 2");
+    });
+
+    it("parses hash comments", () => {
+      test("SELECT 1 + # comment 1\n # comment 2\n 2");
+    });
+
+    it("parses block comments", () => {
+      test("SELECT 1 + /* comment 1 */ /* comment 2 */ 2");
+    });
+
+    it("discards comments when preserveComments option is not set", () => {
+      expect(
+        show(
+          parse("SELECT 1 + /* comment 1 */ /* comment 2 */ 2", {
+            preserveNewlines: true,
+            preserveSpaces: true,
+          })
+        )
+      ).toBe("SELECT 1 +   2");
+    });
+  });
+});
