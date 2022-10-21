b5a956dfa80c1ed7322470f2305c2c372fe2886a
Rene Saarsoo <nene@triin.net>
2022-10-21 21:58:28 +0300

    Support empty window definition

--- test/window_func.test.ts
+++ test/window_func.test.ts
@@ -1,7 +1,12 @@
 import { test } from "./test_utils";
 
 describe("window functions", () => {
-  it("supports basic window function", () => {
+  it("supports empty window specification", () => {
+    test(`SELECT row_number() OVER ()`);
+    test(`SELECT row_number() OVER (/*c*/)`);
+  });
+
+  it("supports ORDER BY", () => {
     test(`SELECT row_number() OVER (ORDER BY col)`);
     test(`SELECT row_number() /*c1*/ OVER /*c2*/ ( /*c3*/ ORDER BY col /*c4*/ )`);
   });
