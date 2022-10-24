c670411005ac7d2cf1cd8ed3e569588f1c3baa57
Rene Saarsoo <nene@triin.net>
2022-10-24 13:18:32 +0300

    Document show() function

--- src/parser.ts
+++ src/parser.ts
@@ -19,6 +19,20 @@ export function parse(
   }
 }
 
+/**
+ * Converts any syntax tree node back to SQL string.
+ *
+ * It's a very primitive serializer that won't insert any whitespace on its own.
+ * It will only restore the whitespace from leading/trailing fields.
+ * Not having this information available can lead to invalid SQL being generated.
+ *
+ * Therefore only feed it syntax trees parsed with options:
+ *
+ *     { preserveSpaces: true, preserveComments: true, preserveNewlines: true }
+ */
 export function show(node: Node): string {
+  // This might look like an unnecessary wrapper around show() from src/show.
+  // The goal here is to restrict the input type to just Node,
+  // not allowing all the additional types that are largely an implementation detail.
   return showSql(node);
 }
