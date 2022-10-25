456947075f8877caac2d8e94a274dfa9c70454f6
Rene Saarsoo <nene@triin.net>
2022-10-25 12:19:58 +0300

    Additional comment tests

--- test/insert.test.ts
+++ test/insert.test.ts
@@ -13,12 +13,14 @@ describe("insert into", () => {
 
   it("supports multiple values", () => {
     test("INSERT INTO tbl VALUES (1, 2, 3), (4, 5, 6), (7, 8, 9)");
+    test("INSERT INTO tbl VALUES (1) /*c1*/,/*c2*/ (2)");
   });
 
   it("supports insert from query", () => {
     test("INSERT INTO tbl SELECT 1, 2, 3");
     test("INSERT INTO tbl (col1, col2, col3) SELECT 1, 2, 3");
     test("INSERT INTO tbl (SELECT 1, 2, 3)");
+    test("INSERT INTO tbl /*c1*/ SELECT 1");
   });
 
   dialect("mysql", () => {
