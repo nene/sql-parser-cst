2dbb33269d3def00ec380a74c5b8c1c1c6b7a0ce
Rene Saarsoo <nene@triin.net>
2022-11-11 13:29:51 +0200

    Extract triggers to sep file

--- src/cst/Node.ts
+++ src/cst/Node.ts
@@ -16,6 +16,7 @@ import {
 import { Literal, NumberLiteral, StringLiteral } from "./Literal";
 import { AllSqliteNodes, SqliteStmt } from "./Sqlite";
 import { AllTransactionNodes, TransactionStmt } from "./Transaction";
+import { AllTriggerNodes, CreateTriggerStmt, DropTriggerStmt } from "./Trigger";
 import { FrameClause, AllFrameNodes } from "./WindowFrame";
 
 export type Node =
@@ -41,9 +42,7 @@ export type Node =
   | OnConflictClause
   | TableOption
   | AlterAction
-  | TriggerEvent
-  | TriggerCondition
-  | TriggerBody
+  | AllTriggerNodes
   | AllColumns
   | AllTransactionNodes
   | CommonTableExpression
@@ -797,51 +796,6 @@ export interface DropIndexStmt extends BaseNode {
   indexes: ListExpr<TableRef>;
   onKw?: Keyword<"ON">;
   table?: TableRef;
-}
-
-// CREATE TRIGGER
-export interface CreateTriggerStmt extends BaseNode {
-  type: "create_trigger_stmt";
-  createKw: Keyword<"CREATE">;
-  temporaryKw?: Keyword<"TEMP" | "TEMPORARY">;
-  triggerKw: Keyword<"TRIGGER">;
-  ifNotExistsKw?: [Keyword<"IF">, Keyword<"NOT">, Keyword<"EXISTS">];
-  name: TableRef;
-  event: TriggerEvent;
-  onKw: Keyword<"ON">;
-  table: TableRef;
-  forEachRowKw?: [Keyword<"FOR">, Keyword<"EACH">, Keyword<"ROW">];
-  condition?: TriggerCondition;
-  body: TriggerBody;
-}
-
-export interface TriggerEvent extends BaseNode {
-  type: "trigger_event";
-  timeKw?: Keyword<"BEFORE" | "AFTER"> | [Keyword<"INSTEAD">, Keyword<"OF">];
-  eventKw: Keyword<"INSERT" | "DELETE" | "UPDATE">;
-  ofKw?: Keyword<"OF">;
-  columns?: ListExpr<ColumnRef>;
-}
-
-export interface TriggerCondition extends BaseNode {
-  type: "trigger_condition";
-  whenKw?: Keyword<"WHEN">;
-  expr: Expr;
-}
-
-export interface TriggerBody extends BaseNode {
-  type: "trigger_body";
-  beginKw: Keyword<"BEGIN">;
-  statements: Statement[];
-  endKw: Keyword<"END">;
-}
-
-// DROP TRIGGER
-export interface DropTriggerStmt extends BaseNode {
-  type: "drop_trigger_stmt";
-  dropTriggerKw: [Keyword<"DROP">, Keyword<"TRIGGER">];
-  ifExistsKw?: [Keyword<"IF">, Keyword<"EXISTS">];
-  trigger: TableRef;
 }
 
 // ANALYZE
---
+++ src/cst/Trigger.ts
@@ -0,0 +1,57 @@
+import { BaseNode, Keyword } from "./Base";
+import { ColumnRef, Expr, ListExpr, TableRef } from "./Expr";
+import { Statement } from "./Node";
+
+export type AllTriggerNodes =
+  | CreateTriggerStmt
+  | DropTriggerStmt
+  | TriggerEvent
+  | TriggerCondition
+  | TriggerBody
+  | TriggerCondition
+  | TriggerBody;
+
+// CREATE TRIGGER
+export interface CreateTriggerStmt extends BaseNode {
+  type: "create_trigger_stmt";
+  createKw: Keyword<"CREATE">;
+  temporaryKw?: Keyword<"TEMP" | "TEMPORARY">;
+  triggerKw: Keyword<"TRIGGER">;
+  ifNotExistsKw?: [Keyword<"IF">, Keyword<"NOT">, Keyword<"EXISTS">];
+  name: TableRef;
+  event: TriggerEvent;
+  onKw: Keyword<"ON">;
+  table: TableRef;
+  forEachRowKw?: [Keyword<"FOR">, Keyword<"EACH">, Keyword<"ROW">];
+  condition?: TriggerCondition;
+  body: TriggerBody;
+}
+
+export interface TriggerEvent extends BaseNode {
+  type: "trigger_event";
+  timeKw?: Keyword<"BEFORE" | "AFTER"> | [Keyword<"INSTEAD">, Keyword<"OF">];
+  eventKw: Keyword<"INSERT" | "DELETE" | "UPDATE">;
+  ofKw?: Keyword<"OF">;
+  columns?: ListExpr<ColumnRef>;
+}
+
+export interface TriggerCondition extends BaseNode {
+  type: "trigger_condition";
+  whenKw?: Keyword<"WHEN">;
+  expr: Expr;
+}
+
+export interface TriggerBody extends BaseNode {
+  type: "trigger_body";
+  beginKw: Keyword<"BEGIN">;
+  statements: Statement[];
+  endKw: Keyword<"END">;
+}
+
+// DROP TRIGGER
+export interface DropTriggerStmt extends BaseNode {
+  type: "drop_trigger_stmt";
+  dropTriggerKw: [Keyword<"DROP">, Keyword<"TRIGGER">];
+  ifExistsKw?: [Keyword<"IF">, Keyword<"EXISTS">];
+  trigger: TableRef;
+}
