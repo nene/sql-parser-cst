9bad23a84055a2850304d71d228bf67392106596
Rene Saarsoo <nene@triin.net>
2022-11-23 11:41:23 +0200

    Additional tests for PIVOT & UNPIVOT as alias names

--- test/select/from.test.ts
+++ test/select/from.test.ts
@@ -13,6 +13,18 @@ describe("select FROM", () => {
     test("SELECT t.col FROM db.tbl /*c1*/ AS /*c2*/ t");
   });
 
+  dialect("bigquery", () => {
+    it("supports PIVOT & UNPIVOT as explicit table aliases", () => {
+      test("SELECT pivot.col FROM my_db.my_long_table_name AS pivot");
+      test("SELECT unpivot.col FROM my_db.my_long_table_name AS unpivot");
+    });
+
+    it.skip("supports PIVOT & UNPIVOT as implicit table aliases", () => {
+      test("SELECT pivot.col FROM my_db.my_long_table_name pivot");
+      test("SELECT unpivot.col FROM my_db.my_long_table_name unpivot");
+    });
+  });
+
   it("supports table name in parenthesis", () => {
     test("SELECT col FROM (tbl)");
     test("SELECT t.col FROM (db.tbl) AS t");
