fb89cc7e93b0f5fc5f7d77c61dadb2bba2ce96b6
Rene Saarsoo <nene@triin.net>
2022-12-09 11:29:03 +0200

    Use testWc to simplify Bigquery-specific tests

--- test/bigquery.test.ts
+++ test/bigquery.test.ts
@@ -1,23 +1,16 @@
-import { dialect, test } from "./test_utils";
+import { dialect, testWc } from "./test_utils";
 
 describe("BigQuery specific statements", () => {
   dialect("bigquery", () => {
     ["CAPACITY", "RESERVATION", "ASSIGNMENT"].forEach((keyword) => {
       it(`supports CREATE ${keyword}`, () => {
-        test(`CREATE ${keyword} project_id.location.commitment_id AS JSON '{}'`);
-        test(`CREATE ${keyword} \`admin_project.region-us.my-commitment\` AS JSON '{}'`);
-        test(`
-          CREATE /*c1*/ ${keyword} /*c2*/ proj.loc.comid /*c3*/
-          AS /*c4*/ JSON /*c5*/ '''{
-            "slot_count": 100,
-            "plan": "FLEX"
-          }'''`);
+        testWc(`CREATE ${keyword} project_id.location.commitment_id AS JSON '{}'`);
+        testWc(`CREATE ${keyword} \`admin_project.region-us.my-commitment\` AS JSON '{}'`);
       });
 
       it(`supports DROP ${keyword}`, () => {
-        test(`DROP ${keyword} project_id.location.commitment_id`);
-        test(`DROP ${keyword} \`admin_project.region-us.my-commitment\``);
-        test(`DROP /*c1*/ ${keyword} /*c2*/ IF /*c3*/ EXISTS /*c4*/ proj.loc.comid`);
+        testWc(`DROP ${keyword} project_id.location.commitment_id`);
+        testWc(`DROP ${keyword} IF EXISTS \`admin_project.region-us.my-commitment\``);
       });
     });
   });
