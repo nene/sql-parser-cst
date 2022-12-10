e11134d126815b8d3c2d6b2f52a249c796c49394
Rene Saarsoo <nene@triin.net>
2022-12-10 18:38:00 +0200

    Use testWc() in transactions test

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -1,39 +1,35 @@
-import { dialect, test } from "./test_utils";
+import { dialect, testWc } from "./test_utils";
 
 describe("transactions", () => {
   describe("starting transaction", () => {
     // standard syntax
     dialect("mysql", () => {
       it("supports START TRANSACTION", () => {
-        test("START TRANSACTION");
-        test("START /*c1*/ TRANSACTION");
+        testWc("START TRANSACTION");
       });
     });
 
     it("supports BEGIN", () => {
-      test("BEGIN");
+      testWc("BEGIN");
     });
 
     dialect(["sqlite", "bigquery"], () => {
       it("supports BEGIN TRANSACTION", () => {
-        test("BEGIN TRANSACTION");
-        test("BEGIN /*c1*/ TRANSACTION");
+        testWc("BEGIN TRANSACTION");
       });
     });
 
     dialect("sqlite", () => {
       it("supports BEGIN kind TRANSACTION", () => {
-        test("BEGIN DEFERRED TRANSACTION");
-        test("BEGIN IMMEDIATE TRANSACTION");
-        test("BEGIN EXCLUSIVE TRANSACTION");
-        test("BEGIN /*c1*/ EXCLUSIVE /*c2*/ TRANSACTION");
+        testWc("BEGIN DEFERRED TRANSACTION");
+        testWc("BEGIN IMMEDIATE TRANSACTION");
+        testWc("BEGIN EXCLUSIVE TRANSACTION");
       });
     });
 
     dialect("mysql", () => {
       it("supports BEGIN WORK", () => {
-        test("BEGIN WORK");
-        test("BEGIN /**/ WORK");
+        testWc("BEGIN WORK");
       });
     });
   });
@@ -41,28 +37,25 @@ describe("transactions", () => {
   describe("committing transaction", () => {
     // standard syntax
     it("supports COMMIT", () => {
-      test("COMMIT");
+      testWc("COMMIT");
     });
 
     dialect(["sqlite", "bigquery"], () => {
       it("supports COMMIT TRANSACTION", () => {
-        test("COMMIT TRANSACTION");
-        test("COMMIT /*c1*/ TRANSACTION");
+        testWc("COMMIT TRANSACTION");
       });
     });
 
     dialect("sqlite", () => {
       it("supports END [TRANSACTION]", () => {
-        test("END TRANSACTION");
-        test("END");
-        test("END /*c1*/ TRANSACTION");
+        testWc("END TRANSACTION");
+        testWc("END");
       });
     });
 
     dialect("mysql", () => {
       it("supports COMMIT WORK", () => {
-        test("COMMIT WORK");
-        test("COMMIT /*c1*/ WORK");
+        testWc("COMMIT WORK");
       });
     });
   });
@@ -70,20 +63,18 @@ describe("transactions", () => {
   describe("rolling back a transaction", () => {
     // standard syntax
     it("supports ROLLBACK", () => {
-      test("ROLLBACK");
+      testWc("ROLLBACK");
     });
 
     dialect(["sqlite", "bigquery"], () => {
       it("supports ROLLBACK TRANSACTION", () => {
-        test("ROLLBACK TRANSACTION");
-        test("ROLLBACK /*c1*/ TRANSACTION");
+        testWc("ROLLBACK TRANSACTION");
       });
     });
 
     dialect("mysql", () => {
       it("supports ROLLBACK WORK", () => {
-        test("ROLLBACK WORK");
-        test("ROLLBACK /*c1*/ WORK");
+        testWc("ROLLBACK WORK");
       });
     });
   });
@@ -91,40 +82,39 @@ describe("transactions", () => {
   dialect(["mysql", "sqlite"], () => {
     describe("creating savepoints", () => {
       it("supports SAVEPOINT", () => {
-        test("SAVEPOINT my_sp");
+        testWc("SAVEPOINT my_sp");
       });
     });
 
     describe("removing savepoints", () => {
       it("supports RELEASE SAVEPOINT", () => {
-        test("RELEASE SAVEPOINT my_sp");
+        testWc("RELEASE SAVEPOINT my_sp");
       });
 
       dialect("sqlite", () => {
         it("supports RELEASE", () => {
-          test("RELEASE my_sp");
+          testWc("RELEASE my_sp");
         });
       });
     });
 
     describe("rolling back to savepoints", () => {
       it("supports ROLLBACK TO [SAVEPOINT]", () => {
-        test("ROLLBACK TO SAVEPOINT my_savepoint");
-        test("ROLLBACK TO my_savepoint");
-        test("ROLLBACK /*c1*/ TO /*c2*/ SAVEPOINT /*c3*/ my_savepoint");
+        testWc("ROLLBACK TO SAVEPOINT my_savepoint");
+        testWc("ROLLBACK TO my_savepoint");
       });
 
       dialect("sqlite", () => {
         it("supports ROLLBACK TRANSACTION TO [SAVEPOINT]", () => {
-          test("ROLLBACK TRANSACTION TO SAVEPOINT my_savepoint");
-          test("ROLLBACK TRANSACTION TO my_savepoint");
+          testWc("ROLLBACK TRANSACTION TO SAVEPOINT my_savepoint");
+          testWc("ROLLBACK TRANSACTION TO my_savepoint");
         });
       });
 
       dialect("mysql", () => {
         it("supports ROLLBACK WORK TO [SAVEPOINT]", () => {
-          test("ROLLBACK WORK TO SAVEPOINT my_savepoint");
-          test("ROLLBACK WORK TO my_savepoint");
+          testWc("ROLLBACK WORK TO SAVEPOINT my_savepoint");
+          testWc("ROLLBACK WORK TO my_savepoint");
         });
       });
     });
