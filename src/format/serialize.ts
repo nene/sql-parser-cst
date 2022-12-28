286da830f6f8cd6d70430471a7fa2386a79e9a4a
Rene Saarsoo <nene@triin.net>
2022-12-28 17:26:10 +0200

    Delete the whole SQL formatting functionality
    This has been moved to: https://github.com/nene/sql-auto-format

--- src/format/serialize.ts
+++
@@ -1,8 +0,0 @@
-import { Line } from "./layout";
-
-export function serialize(lines: Line[]): string {
-  const INDENT = "  ";
-  return lines
-    .map((line) => INDENT.repeat(line.indent || 0) + line.items.join(""))
-    .join("\n");
-}
