b3087c20c3766669f79d6ad0bfb9ccf49a262449
Rene Saarsoo <nene@triin.net>
2023-02-11 23:34:27 +0200

    Strip undefined fields from AST for testing

--- test/ast/ast_test_utils.ts
+++ test/ast/ast_test_utils.ts
@@ -1,7 +1,38 @@
 import { ParserOptions } from "../../src/main";
 import { cstToAst } from "../../src/cstToAst";
 import { parse } from "../test_utils";
+import { Node } from "../../src/ast/Node";
+import { isObject, isString } from "../../src/utils/generic";
 
 export function parseAst(sql: string, options: Partial<ParserOptions> = {}) {
-  return cstToAst(parse(sql, options));
+  return stripUndefinedFields(cstToAst(parse(sql, options)));
 }
+
+function stripUndefinedFields<T extends Node>(ast: T): T {
+  visitAllAstNodes(ast, (node: Record<any, any>) => {
+    for (const key of Object.keys(node)) {
+      if (node[key] === undefined) {
+        delete node[key];
+      }
+    }
+  });
+  return ast;
+}
+
+/* Executes a function with all nodes in syntax tree */
+const visitAllAstNodes = (node: Node, visit: (node: Node) => void) => {
+  visit(node);
+
+  // Visit all children
+  for (const child of Object.values(node)) {
+    if (isNode(child)) {
+      visitAllAstNodes(child, visit);
+    } else if (Array.isArray(child)) {
+      child
+        .filter(isNode)
+        .forEach((childNode) => visitAllAstNodes(childNode, visit));
+    }
+  }
+};
+
+const isNode = (obj: any): obj is Node => isObject(obj) && isString(obj.type);
--- test/ast/select.test.ts
+++ test/ast/select.test.ts
@@ -4,33 +4,26 @@ describe("select", () => {
   it("parses simple SELECT", () => {
     expect(parseAst("SELECT id, 'hello' + 1")).toMatchInlineSnapshot(`
       {
-        "range": undefined,
         "statements": [
           {
             "columns": [
               {
                 "name": "id",
-                "range": undefined,
                 "type": "identifier",
               },
               {
                 "left": {
-                  "range": undefined,
                   "type": "string_literal",
                   "value": "hello",
                 },
                 "operator": "+",
-                "range": undefined,
                 "right": {
-                  "range": undefined,
                   "type": "string_literal",
                   "value": "hello",
                 },
                 "type": "binary_expr",
               },
             ],
-            "distinct": undefined,
-            "range": undefined,
             "type": "select_stmt",
           },
         ],
