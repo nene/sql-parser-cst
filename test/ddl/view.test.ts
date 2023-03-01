f6200dfe8ee8c909ccb6d4b23b25ba47de2d094b
Rene Saarsoo <nene@triin.net>
2023-03-01 15:41:21 +0200

    Use testWc() in view tests

--- test/ddl/view.test.ts
+++ test/ddl/view.test.ts
@@ -1,71 +1,62 @@
-import { dialect, test } from "../test_utils";
+import { dialect, testWc } from "../test_utils";
 
 describe("view", () => {
   describe("CREATE VIEW", () => {
     it("simple CREATE VIEW statement", () => {
-      test("CREATE VIEW my_view AS SELECT * FROM tbl");
-      test("CREATE VIEW schemata.my_view AS (SELECT * FROM tbl)");
-      test("CREATE /*c1*/ VIEW /*c2*/ my_view /*c3*/ AS /*c4*/ SELECT * FROM tbl");
+      testWc("CREATE VIEW my_view AS SELECT * FROM tbl");
+      testWc("CREATE VIEW schemata.my_view AS (SELECT * FROM tbl)");
     });
 
     it("supports columns list", () => {
-      test("CREATE VIEW my_view (col1, col2) AS SELECT 1, 2");
-      test(
-        "CREATE VIEW my_view /*c1*/(/*c2*/ col1 /*c3*/,/*c4*/ col2 /*c5*/)/*c6*/ AS SELECT 1, 2"
-      );
+      testWc("CREATE VIEW my_view (col1, col2) AS SELECT 1, 2");
     });
 
     dialect(["mysql", "bigquery"], () => {
       it("supports OR REPLACE", () => {
-        test("CREATE OR REPLACE VIEW my_view AS SELECT 1");
-        test("CREATE /*c1*/ OR /*c2*/ REPLACE /*c3*/ VIEW my_view AS SELECT 1");
+        testWc("CREATE OR REPLACE VIEW my_view AS SELECT 1");
       });
     });
 
     dialect("sqlite", () => {
       it("TEMPORARY view", () => {
-        test("CREATE TEMP VIEW my_view AS SELECT 1");
-        test("CREATE TEMPORARY VIEW my_view AS SELECT 1");
-        test("CREATE /*c1*/ TEMP /*c2*/ VIEW my_view AS SELECT 1");
+        testWc("CREATE TEMP VIEW my_view AS SELECT 1");
+        testWc("CREATE TEMPORARY VIEW my_view AS SELECT 1");
       });
     });
 
     dialect(["sqlite", "bigquery"], () => {
       it("supports IF NOT EXISTS", () => {
-        test("CREATE VIEW IF NOT EXISTS my_view AS SELECT 1");
-        test("CREATE VIEW /*c1*/ IF /*c2*/ NOT /*c3*/ EXISTS /*c4*/ my_view AS SELECT 1");
+        testWc("CREATE VIEW IF NOT EXISTS my_view AS SELECT 1");
       });
     });
 
     dialect("bigquery", () => {
       it("supports OPTIONS(...)", () => {
-        test("CREATE VIEW my_view OPTIONS(description = 'blah') AS SELECT 1");
-        test("CREATE VIEW my_view (col1, col2) OPTIONS(description = 'blah') AS SELECT 1");
-        test("CREATE VIEW my_view /*c1*/ OPTIONS(description = 'blah') /*c2*/ AS SELECT 1");
+        testWc("CREATE VIEW my_view OPTIONS(description = 'blah') AS SELECT 1");
+        testWc("CREATE VIEW my_view (col1, col2) OPTIONS(description = 'blah') AS SELECT 1");
       });
     });
 
     dialect("bigquery", () => {
       describe("materialized view", () => {
         it("supports CREATE MATERIALIZED VIEW", () => {
-          test("CREATE MATERIALIZED VIEW my_view AS SELECT 1");
-          test("CREATE /*c1*/ MATERIALIZED /*c2*/ VIEW my_view AS SELECT 1");
+          testWc("CREATE MATERIALIZED VIEW my_view AS SELECT 1");
         });
 
         it("supports PARTITION BY option", () => {
-          test("CREATE MATERIALIZED VIEW my_view PARTITION BY timestamp_col AS SELECT 1");
+          testWc("CREATE MATERIALIZED VIEW my_view PARTITION BY timestamp_col AS SELECT 1");
         });
 
         it("supports CLUSTER BY option", () => {
-          test("CREATE MATERIALIZED VIEW my_view CLUSTER BY col1, col2 AS SELECT 1");
+          testWc("CREATE MATERIALIZED VIEW my_view CLUSTER BY col1, col2 AS SELECT 1");
         });
 
         it("supports OPTIONS(...)", () => {
-          test("CREATE MATERIALIZED VIEW my_view OPTIONS(description='Hi') AS SELECT 1");
+          testWc("CREATE MATERIALIZED VIEW my_view OPTIONS(description='Hi') AS SELECT 1");
         });
 
         it("supports combination of multiple view options", () => {
-          test(`
+          testWc(`
             CREATE MATERIALIZED VIEW my_view
             PARTITION BY timestamp_col
             CLUSTER BY col1, col2
@@ -79,33 +70,28 @@ describe("view", () => {
 
   describe("DROP VIEW", () => {
     it("simple DROP VIEW statement", () => {
-      test("DROP VIEW my_view");
-      test("DROP VIEW schemata.my_view");
-      test("DROP /*c1*/ VIEW /*c2*/ my_view");
+      testWc("DROP VIEW my_view");
+      testWc("DROP VIEW schemata.my_view");
     });
 
     it("supports IF EXISTS", () => {
-      test("DROP VIEW IF EXISTS my_view");
-      test("DROP VIEW /*c1*/ IF /*c2*/ EXISTS /*c4*/ my_view");
+      testWc("DROP VIEW IF EXISTS my_view");
     });
 
     it("supports multiple views", () => {
-      test("DROP VIEW view1, view2, view3");
-      test("DROP VIEW view1 /*c1*/,/*c2*/ view2");
+      testWc("DROP VIEW view1, view2, view3");
     });
 
     dialect("bigquery", () => {
       it("supports DROP MATERIALIZED VIEW", () => {
-        test("DROP MATERIALIZED VIEW my_view");
-        test("DROP /*c1*/ MATERIALIZED /*c2*/ VIEW my_view");
+        testWc("DROP MATERIALIZED VIEW my_view");
       });
     });
 
     dialect("mysql", () => {
       it("with CASCADE/RESTRICT behavior", () => {
-        test("DROP VIEW v1 CASCADE");
-        test("DROP VIEW v1 RESTRICT");
-        test("DROP VIEW v1 /*c1*/ RESTRICT");
+        testWc("DROP VIEW v1 CASCADE");
+        testWc("DROP VIEW v1 RESTRICT");
       });
     });
   });
@@ -113,17 +99,13 @@ describe("view", () => {
   describe("ALTER VIEW", () => {
     dialect("bigquery", () => {
       it("supports ALTER VIEW [IF EXISTS] .. SET OPTIONS()", () => {
-        test("ALTER VIEW my_view SET OPTIONS(description='blah')");
-        test("ALTER VIEW IF EXISTS my_view SET OPTIONS(description='blah')");
+        testWc("ALTER VIEW my_view SET OPTIONS(description='blah')");
+        testWc("ALTER VIEW IF EXISTS my_view SET OPTIONS(description='blah')");
       });
 
       it("supports ALTER MATERIALIZED VIEW [IF EXISTS] .. SET OPTIONS()", () => {
-        test("ALTER MATERIALIZED VIEW my_view SET OPTIONS(description='blah')");
-        test("ALTER MATERIALIZED VIEW IF EXISTS my_view SET OPTIONS(description='blah')");
-        test(`
-          ALTER /*c1*/ MATERIALIZED /*c2*/ VIEW /*c3*/ IF /*c4*/ EXISTS /*c5*/ my_view /*c6*/
-          SET /*c7*/ OPTIONS /*c8*/ (description='blah')
-        `);
+        testWc("ALTER MATERIALIZED VIEW my_view SET OPTIONS(description='blah')");
+        testWc("ALTER MATERIALIZED VIEW IF EXISTS my_view SET OPTIONS(description='blah')");
       });
     });
   });
