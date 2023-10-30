d6f863d807ba5e689a4849e7b258d729045bf318
Rene Saarsoo <nene@triin.net>
2023-10-30 11:57:54 +0200

    Test that VALUES clause is supported in Postgres

--- test/select/values.test.ts
+++ test/select/values.test.ts
@@ -1,7 +1,7 @@
 import { dialect, test, testWc } from "../test_utils";
 
 describe("VALUES clause/statement", () => {
-  dialect("sqlite", () => {
+  dialect(["sqlite", "postgresql"], () => {
     it("parses separate VALUES statement", () => {
       testWc("VALUES (1, 'Hello', TRUE, NULL)");
       testWc("VALUES (1, 'Hello'), (2, 'World')");
@@ -9,6 +9,18 @@ describe("VALUES clause/statement", () => {
 
     it("parses UNION of VALUES", () => {
       testWc("VALUES (1, 2) UNION VALUES (3, 4)");
+    });
+  });
+
+  // The following should be also valid in SQLite according to documentation,
+  // but in practice it produces a syntax error.
+  dialect("postgresql", () => {
+    it("supports VALUES followed by ORDER BY", () => {
+      testWc("VALUES (1, 2), (3, 4) ORDER BY 2");
+    });
+
+    it("supports VALUES followed by LIMIT", () => {
+      testWc("VALUES (1, 2), (3, 4) LIMIT 1");
     });
   });
 
@@ -26,12 +38,6 @@ describe("VALUES clause/statement", () => {
   dialect("bigquery", () => {
     it("does not support a separate VALUES statement", () => {
       expect(() => test("VALUES (1, 2)")).toThrowError();
-    });
-  });
-
-  dialect("postgresql", () => {
-    it.skip("TODO:postgres", () => {
-      expect(true).toBe(true);
     });
   });
 });
