83a6c19a8fce901dbf3b146e193982a2467b1570
Rene Saarsoo <nene@triin.net>
2024-01-26 15:39:03 +0200

    Unify PostgreSQL and BigQuery CREATE FUNCTION tests

--- test/ddl/function.test.ts
+++ test/ddl/function.test.ts
@@ -1,158 +1,158 @@
 import { dialect, test, testWc } from "../test_utils";
 
 describe("function", () => {
-  dialect(["bigquery"], () => {
+  function testCreateFunction(body: string) {
     describe("CREATE FUNCTION", () => {
       it("supports basic CREATE FUNCTION", () => {
-        testWc("CREATE FUNCTION foo ( ) AS (1 * 2)");
-        testWc("CREATE FUNCTION foo.bar.baz ( ) AS (1)");
+        testWc(`CREATE FUNCTION foo ( ) ${body}`);
+        testWc(`CREATE FUNCTION foo.bar.baz ( ) ${body}`);
       });
 
       it("supports parameters", () => {
-        testWc("CREATE FUNCTION multiplicate ( x INT , y INT ) AS (x * y)");
+        testWc(`CREATE FUNCTION multiplicate ( x INT , y INT ) ${body}`);
       });
 
       it("supports OR REPLACE", () => {
-        testWc("CREATE OR REPLACE FUNCTION foo() AS (1)");
-      });
-
-      it("supports TEMPORARY FUNCTION", () => {
-        testWc("CREATE TEMP FUNCTION foo() AS (1)");
-        testWc("CREATE TEMPORARY FUNCTION foo() AS (1)");
-      });
-
-      it("supports IF NOT EXISTS", () => {
-        testWc("CREATE FUNCTION IF NOT EXISTS foo() AS (1)");
+        testWc(`CREATE OR REPLACE FUNCTION foo() ${body}`);
       });
 
       it("supports RETURNS", () => {
-        testWc("CREATE FUNCTION foo() RETURNS INT AS (1)");
+        testWc(`CREATE FUNCTION foo() RETURNS INT ${body}`);
       });
 
-      it("supports OPTIONS(..)", () => {
-        testWc("CREATE FUNCTION foo() OPTIONS (description='hello') AS (1)");
-        testWc("CREATE FUNCTION foo() AS (1) OPTIONS (description='my func')");
-      });
-
-      describe("JS functions", () => {
-        it("supports LANGUAGE js", () => {
-          testWc("CREATE FUNCTION foo() RETURNS INT LANGUAGE js AS 'return(x*y);'");
+      dialect("bigquery", () => {
+        it("supports TEMPORARY FUNCTION", () => {
+          testWc("CREATE TEMP FUNCTION foo() AS (1)");
+          testWc("CREATE TEMPORARY FUNCTION foo() AS (1)");
         });
 
-        it("supports DETERMINISTIC / NOT DETERMINISTIC", () => {
-          testWc(`CREATE FUNCTION foo() RETURNS STRING DETERMINISTIC LANGUAGE js AS 'return("");'`);
-          testWc(`CREATE FUNCTION foo() RETURNS INT NOT DETERMINISTIC LANGUAGE js AS 'return(0);'`);
+        it("supports IF NOT EXISTS", () => {
+          testWc("CREATE FUNCTION IF NOT EXISTS foo() AS (1)");
         });
 
         it("supports OPTIONS(..)", () => {
-          testWc(
-            "CREATE FUNCTION foo() RETURNS INT LANGUAGE js OPTIONS (foo=15) AS 'return(x*y);'"
-          );
-          testWc("CREATE FUNCTION foo() RETURNS INT LANGUAGE js AS 'return(x*y);' OPTIONS(foo=2)");
+          testWc("CREATE FUNCTION foo() OPTIONS (description='hello') AS (1)");
+          testWc("CREATE FUNCTION foo() AS (1) OPTIONS (description='my func')");
+        });
+
+        describe("JS functions", () => {
+          it("supports LANGUAGE js", () => {
+            testWc("CREATE FUNCTION foo() RETURNS INT LANGUAGE js AS 'return(x*y);'");
+          });
+
+          it("supports DETERMINISTIC / NOT DETERMINISTIC", () => {
+            testWc(
+              `CREATE FUNCTION foo() RETURNS STRING DETERMINISTIC LANGUAGE js AS 'return("");'`
+            );
+            testWc(
+              `CREATE FUNCTION foo() RETURNS INT NOT DETERMINISTIC LANGUAGE js AS 'return(0);'`
+            );
+          });
+
+          it("supports OPTIONS(..)", () => {
+            testWc(
+              "CREATE FUNCTION foo() RETURNS INT LANGUAGE js OPTIONS (foo=15) AS 'return(x*y);'"
+            );
+            testWc(
+              "CREATE FUNCTION foo() RETURNS INT LANGUAGE js AS 'return(x*y);' OPTIONS(foo=2)"
+            );
+          });
+        });
+
+        describe("remote functions", () => {
+          it("supports REMOTE WITH CONNECTION", () => {
+            testWc("CREATE FUNCTION foo() RETURNS INT REMOTE WITH CONNECTION myconnection_id");
+          });
+
+          it("supports OPTIONS(..)", () => {
+            testWc(`
+              CREATE FUNCTION multiply()
+              RETURNS INT REMOTE WITH CONNECTION myproj.us.myconnection
+              OPTIONS(endpoint="https://example.com/multiply")
+            `);
+          });
+        });
+
+        describe("table functions", () => {
+          it("supports CREATE TABLE FUNCTION", () => {
+            testWc("CREATE TABLE FUNCTION foo() AS SELECT * FROM tbl");
+            testWc("CREATE OR REPLACE TABLE FUNCTION foo() AS SELECT 1");
+            testWc("CREATE TABLE FUNCTION IF NOT EXISTS foo() AS SELECT 1");
+          });
+
+          it("supports RETURNS TABLE <..>", () => {
+            testWc("CREATE TABLE FUNCTION foo() RETURNS TABLE < col1 INT64 > AS SELECT 1");
+            testWc(`
+              CREATE TABLE FUNCTION foo()
+              RETURNS TABLE< name STRING , age INT >
+              AS SELECT 'John', 64
+            `);
+          });
+
+          it("supports parameters with ANY TYPE", () => {
+            testWc(`
+              CREATE TABLE FUNCTION foo( p ANY TYPE )
+              AS SELECT * FROM tbl WHERE col = p
+            `);
+          });
+
+          it("supports OPTIONS(..)", () => {
+            testWc(`
+              CREATE TABLE FUNCTION doubleit(x INT)
+              RETURNS TABLE<x INT64>
+              OPTIONS(description='haha')
+              AS SELECT x*2
+            `);
+          });
         });
       });
 
-      describe("remote functions", () => {
-        it("supports REMOTE WITH CONNECTION", () => {
-          testWc("CREATE FUNCTION foo() RETURNS INT REMOTE WITH CONNECTION myconnection_id");
+      dialect(["postgresql"], () => {
+        describe("begin..end block", () => {
+          it("supports BEGIN ATOMIC .. END block", () => {
+            testWc(`
+              CREATE FUNCTION foo() RETURNS INT
+              BEGIN ATOMIC
+                SELECT 1;
+              END
+            `);
+          });
+
+          it("supports RETURN inside begin..end block", () => {
+            testWc(`
+              CREATE FUNCTION foo() RETURNS INT
+              BEGIN ATOMIC
+                SELECT * INTO result FROM bar;
+                RETURN result;
+              END
+            `);
+          });
         });
 
-        it("supports OPTIONS(..)", () => {
-          testWc(`
-            CREATE FUNCTION multiply()
-            RETURNS INT REMOTE WITH CONNECTION myproj.us.myconnection
-            OPTIONS(endpoint="https://example.com/multiply")
-          `);
-        });
-      });
+        describe("functions in other languages", () => {
+          it("supports LANGUAGE SQL", () => {
+            testWc("CREATE FUNCTION foo() RETURNS INT LANGUAGE SQL AS 'SELECT 1'");
+          });
 
-      describe("table functions", () => {
-        it("supports CREATE TABLE FUNCTION", () => {
-          testWc("CREATE TABLE FUNCTION foo() AS SELECT * FROM tbl");
-          testWc("CREATE OR REPLACE TABLE FUNCTION foo() AS SELECT 1");
-          testWc("CREATE TABLE FUNCTION IF NOT EXISTS foo() AS SELECT 1");
-        });
-
-        it("supports RETURNS TABLE <..>", () => {
-          testWc("CREATE TABLE FUNCTION foo() RETURNS TABLE < col1 INT64 > AS SELECT 1");
-          testWc(`
-            CREATE TABLE FUNCTION foo()
-            RETURNS TABLE< name STRING , age INT >
-            AS SELECT 'John', 64
-          `);
-        });
-
-        it("supports parameters with ANY TYPE", () => {
-          testWc(`
-            CREATE TABLE FUNCTION foo( p ANY TYPE )
-            AS SELECT * FROM tbl WHERE col = p
-          `);
-        });
-
-        it("supports OPTIONS(..)", () => {
-          testWc(`
-            CREATE TABLE FUNCTION doubleit(x INT)
-            RETURNS TABLE<x INT64>
-            OPTIONS(description='haha')
-            AS SELECT x*2
-          `);
+          it("supports LANGUAGE PL/pgSQL", () => {
+            testWc(`
+              CREATE FUNCTION foo() RETURNS INT AS $$
+                BEGIN
+                  RETURN 1;
+                END;
+              $$ LANGUAGE plpgsql;
+            `);
+          });
         });
       });
     });
+  }
+
+  dialect("bigquery", () => {
+    testCreateFunction("AS (1 * 2)");
   });
-
-  dialect(["postgresql"], () => {
-    describe("CREATE FUNCTION", () => {
-      it("supports basic CREATE FUNCTION", () => {
-        testWc("CREATE FUNCTION foo ( ) RETURNS INT RETURN 1 * 2");
-        testWc("CREATE FUNCTION schm.baz ( ) RETURNS INT RETURN 1");
-      });
-
-      it("supports parameters", () => {
-        testWc("CREATE FUNCTION multiplicate ( x INT , y INT ) RETURNS INT RETURN (x * y)");
-      });
-
-      it("supports OR REPLACE", () => {
-        testWc("CREATE OR REPLACE FUNCTION foo() RETURNS INT RETURN 1");
-      });
-
-      describe("begin..end block", () => {
-        it("supports BEGIN ATOMIC .. END block", () => {
-          testWc(`
-            CREATE FUNCTION foo() RETURNS INT
-            BEGIN ATOMIC
-              SELECT 1;
-            END
-          `);
-        });
-
-        it("supports RETURN inside begin..end block", () => {
-          testWc(`
-            CREATE FUNCTION foo() RETURNS INT
-            BEGIN ATOMIC
-              SELECT * INTO result FROM bar;
-              RETURN result;
-            END
-          `);
-        });
-      });
-
-      describe("functions in other languages", () => {
-        it("supports LANGUAGE SQL", () => {
-          testWc("CREATE FUNCTION foo() RETURNS INT LANGUAGE SQL AS 'SELECT 1'");
-        });
-
-        it("supports LANGUAGE PL/pgSQL", () => {
-          testWc(`
-            CREATE FUNCTION foo() RETURNS INT AS $$
-              BEGIN
-                RETURN 1;
-              END;
-            $$ LANGUAGE plpgsql;
-          `);
-        });
-      });
-    });
+  dialect("postgresql", () => {
+    testCreateFunction("RETURN 1 * 2");
   });
 
   dialect(["bigquery", "postgresql"], () => {
