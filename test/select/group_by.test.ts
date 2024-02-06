6e2b3c172c3237ac20fccc2645e08e13a92011a6
Rene Saarsoo <nene@triin.net>
2024-02-06 17:32:10 +0200

    Enable multi CUBE/ROLLUP tests also for BigQuery

--- test/select/group_by.test.ts
+++ test/select/group_by.test.ts
@@ -31,19 +31,17 @@ describe("select GROUP BY", () => {
       testClauseWc("GROUP BY CUBE (id, (name, age))");
     });
 
-    dialect("postgresql", () => {
-      it("supports multiple ROLLUPs and CUBEs in one GROUP BY", () => {
-        testClauseWc("GROUP BY ROLLUP(id), CUBE(name)");
-      });
+    it("supports multiple ROLLUPs and CUBEs in one GROUP BY", () => {
+      testClauseWc("GROUP BY ROLLUP(id), CUBE(name)");
+    });
 
-      it("parses multiple ROLLUPs & CUBEs as group_by_rollup/cube nodes", () => {
-        const clause = parseClause("GROUP BY ROLLUP(id), CUBE(name)");
-        if (clause.type !== "group_by_clause") {
-          throw new Error("Expected group_by_clause");
-        }
-        expect(clause.columns.items[0].type).toBe("group_by_rollup");
-        expect(clause.columns.items[1].type).toBe("group_by_cube");
-      });
+    it("parses multiple ROLLUPs & CUBEs as group_by_rollup/cube nodes", () => {
+      const clause = parseClause("GROUP BY ROLLUP(id), CUBE(name)");
+      if (clause.type !== "group_by_clause") {
+        throw new Error("Expected group_by_clause");
+      }
+      expect(clause.columns.items[0].type).toBe("group_by_rollup");
+      expect(clause.columns.items[1].type).toBe("group_by_cube");
     });
   });
 
