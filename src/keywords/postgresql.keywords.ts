35b98f16645263d1da7294a258dcbd66903ae87e
Rene Saarsoo <nene@triin.net>
2025-12-25 13:32:08 +0200

    Reorganize PostgreSQL keyword list as a map of various keyword types

--- src/keywords/postgresql.keywords.ts
+++ src/keywords/postgresql.keywords.ts
@@ -1,169 +1,183 @@
-// https://www.postgresql.org/docs/16/sql-keywords-appendix.html
-export const postgresqlKeywords: Record<string, boolean> = {
-  ALL: true, // reserved
-  ANALYSE: true, // reserved
-  ANALYZE: true, // reserved
-  AND: true, // reserved
-  ANY: true, // reserved
-  ARRAY: true, // reserved, requires AS
-  AS: true, // reserved, requires AS
-  ASC: true, // reserved
-  ASYMMETRIC: true, // reserved
-  AUTHORIZATION: true, // reserved (can be function or type)
-  // BETWEEN: true, // (cannot be function or type)
-  // BIGINT: true, // (cannot be function or type)
-  // BINARY: true, // reserved (can be function or type)
-  // BIT: true, // (cannot be function or type)
-  // BOOLEAN: true, // (cannot be function or type)
-  BOTH: true, // reserved
-  CASE: true, // reserved
-  CAST: true, // reserved
-  // CHAR: true, // (cannot be function or type), requires AS
-  // CHARACTER: true, // (cannot be function or type), requires AS
-  CHECK: true, // reserved
-  // COALESCE: true, // (cannot be function or type)
-  COLLATE: true, // reserved
-  // COLLATION: true, // reserved (can be function or type)
-  COLUMN: true, // reserved
-  // CONCURRENTLY: true, // reserved (can be function or type)
-  CONSTRAINT: true, // reserved
-  CREATE: true, // reserved, requires AS
-  CROSS: true, // reserved (can be function or type)
-  CURRENT_CATALOG: true, // reserved
-  CURRENT_DATE: true, // reserved
-  CURRENT_ROLE: true, // reserved
-  CURRENT_SCHEMA: true, // reserved (can be function or type)
-  CURRENT_TIME: true, // reserved
-  CURRENT_TIMESTAMP: true, // reserved
-  CURRENT_USER: true, // reserved
-  DAY: true, // requires AS
-  // DEC: true, // (cannot be function or type)
-  // DECIMAL: true, // (cannot be function or type)
-  DEFAULT: true, // reserved
-  DEFERRABLE: true, // reserved
-  DESC: true, // reserved
-  DISTINCT: true, // reserved
-  DO: true, // reserved
-  ELSE: true, // reserved
-  END: true, // reserved
-  EXCEPT: true, // reserved, requires AS
-  // EXISTS: true, // (cannot be function or type)
-  // EXTRACT: true, // (cannot be function or type)
-  FALSE: true, // reserved
-  FETCH: true, // reserved, requires AS
-  FILTER: true, // requires AS
-  // FLOAT: true, // (cannot be function or type)
-  FOR: true, // reserved, requires AS
-  FOREIGN: true, // reserved
-  FREEZE: true, // reserved (can be function or type)
-  FROM: true, // reserved, requires AS
-  FULL: true, // reserved (can be function or type)
-  GRANT: true, // reserved, requires AS
-  // GREATEST: true, // (cannot be function or type)
-  GROUP: true, // reserved, requires AS
-  // GROUPING: true, // (cannot be function or type)
-  HAVING: true, // reserved, requires AS
-  HOUR: true, // requires AS
-  ILIKE: true, // reserved (can be function or type)
-  IN: true, // reserved
-  INITIALLY: true, // reserved
-  INNER: true, // reserved (can be function or type)
-  // INOUT: true, // (cannot be function or type)
-  // INT: true, // (cannot be function or type)
-  // INTEGER: true, // (cannot be function or type)
-  INTERSECT: true, // reserved, requires AS
-  // INTERVAL: true, // (cannot be function or type)
-  INTO: true, // reserved, requires AS
-  IS: true, // reserved (can be function or type)
-  ISNULL: true, // reserved (can be function or type), requires AS
-  JOIN: true, // reserved (can be function or type)
-  LATERAL: true, // reserved
-  LEADING: true, // reserved
-  // LEAST: true, // (cannot be function or type)
-  LEFT: true, // reserved (can be function or type)
-  LIKE: true, // reserved (can be function or type)
-  LIMIT: true, // reserved, requires AS
-  LOCALTIME: true, // reserved
-  LOCALTIMESTAMP: true, // reserved
-  MINUTE: true, // requires AS
-  MONTH: true, // requires AS
-  // NATIONAL: true, // (cannot be function or type)
-  NATURAL: true, // reserved (can be function or type)
-  // NCHAR: true, // (cannot be function or type)
-  // NONE: true, // (cannot be function or type)
-  // NORMALIZE: true, // (cannot be function or type)
-  NOT: true, // reserved
-  NOTNULL: true, // reserved (can be function or type), requires AS
-  NULL: true, // reserved
-  // NULLIF: true, // (cannot be function or type)
-  // NUMERIC: true, // (cannot be function or type)
-  OFFSET: true, // reserved, requires AS
-  ON: true, // reserved, requires AS
-  ONLY: true, // reserved
-  OR: true, // reserved
-  ORDER: true, // reserved, requires AS
-  // OUT: true, // (cannot be function or type)
-  OUTER: true, // reserved (can be function or type)
-  OVER: true, // requires AS
-  OVERLAPS: true, // reserved (can be function or type), requires AS
-  // OVERLAY: true, // (cannot be function or type)
-  PLACING: true, // reserved
-  // POSITION: true, // (cannot be function or type)
-  PRECISION: true, // (cannot be function or type), requires AS
-  PRIMARY: true, // reserved
-  // REAL: true, // (cannot be function or type)
-  REFERENCES: true, // reserved
-  RETURNING: true, // reserved, requires AS
-  RIGHT: true, // reserved (can be function or type)
-  // ROW: true, // (cannot be function or type)
-  SECOND: true, // requires AS
-  SELECT: true, // reserved
-  SESSION_USER: true, // reserved
-  // SETOF: true, // (cannot be function or type)
-  SIMILAR: true, // reserved (can be function or type)
-  // SMALLINT: true, // (cannot be function or type)
-  SOME: true, // reserved
-  // SUBSTRING: true, // (cannot be function or type)
-  SYMMETRIC: true, // reserved
-  SYSTEM_USER: true, // reserved
-  TABLE: true, // reserved
-  TABLESAMPLE: true, // reserved (can be function or type)
-  THEN: true, // reserved
-  // TIME: true, // (cannot be function or type)
-  // TIMESTAMP: true, // (cannot be function or type)
-  TO: true, // reserved, requires AS
-  TRAILING: true, // reserved
-  // TREAT: true, // (cannot be function or type)
-  // TRIM: true, // (cannot be function or type)
-  TRUE: true, // reserved
-  UNION: true, // reserved, requires AS
-  UNIQUE: true, // reserved
-  USER: true, // reserved
-  USING: true, // reserved
-  // VALUES: true, // (cannot be function or type)
-  // VARCHAR: true, // (cannot be function or type)
-  VARIADIC: true, // reserved
-  VARYING: true, // requires AS
-  VERBOSE: true, // reserved (can be function or type)
-  WHEN: true, // reserved
-  WHERE: true, // reserved, requires AS
-  WINDOW: true, // reserved, requires AS
-  WITH: true, // reserved, requires AS
-  WITHIN: true, // requires AS
-  WITHOUT: true, // requires AS
-  // XMLATTRIBUTES: true, // (cannot be function or type)
-  // XMLCONCAT: true, // (cannot be function or type)
-  // XMLELEMENT: true, // (cannot be function or type)
-  // XMLEXISTS: true, // (cannot be function or type)
-  // XMLFOREST: true, // (cannot be function or type)
-  // XMLNAMESPACES: true, // (cannot be function or type)
-  // XMLPARSE: true, // (cannot be function or type)
-  // XMLPI: true, // (cannot be function or type)
-  // XMLROOT: true, // (cannot be function or type)
-  // XMLSERIALIZE: true, // (cannot be function or type)
-  // XMLTABLE: true, // (cannot be function or type)
-  YEAR: true, // requires AS
+type KeywordDef = {
+  reserved?: boolean;
+  requiresAs?: boolean;
+  cannotBeFunctionOrType?: boolean;
+  canBeFunctionOrType?: boolean;
+};
 
+// https://www.postgresql.org/docs/16/sql-keywords-appendix.html
+const keywordDefs: Record<string, KeywordDef> = {
+  ALL: { reserved: true },
+  ANALYSE: { reserved: true },
+  ANALYZE: { reserved: true },
+  AND: { reserved: true },
+  ANY: { reserved: true },
+  ARRAY: { reserved: true, requiresAs: true },
+  AS: { reserved: true, requiresAs: true },
+  ASC: { reserved: true },
+  ASYMMETRIC: { reserved: true },
+  AUTHORIZATION: { reserved: true, canBeFunctionOrType: true },
+  BETWEEN: { cannotBeFunctionOrType: true },
+  BIGINT: { cannotBeFunctionOrType: true },
+  BINARY: { reserved: true, canBeFunctionOrType: true },
+  BIT: { cannotBeFunctionOrType: true },
+  BOOLEAN: { cannotBeFunctionOrType: true },
+  BOTH: { reserved: true },
+  CASE: { reserved: true },
+  CAST: { reserved: true },
+  CHAR: { cannotBeFunctionOrType: true, requiresAs: true },
+  CHARACTER: { cannotBeFunctionOrType: true, requiresAs: true },
+  CHECK: { reserved: true },
+  COALESCE: { cannotBeFunctionOrType: true },
+  COLLATE: { reserved: true },
+  COLLATION: { reserved: true, canBeFunctionOrType: true },
+  COLUMN: { reserved: true },
+  CONCURRENTLY: { reserved: true, canBeFunctionOrType: true },
+  CONSTRAINT: { reserved: true },
+  CREATE: { reserved: true, requiresAs: true },
+  CROSS: { reserved: true, canBeFunctionOrType: true },
+  CURRENT_CATALOG: { reserved: true },
+  CURRENT_DATE: { reserved: true },
+  CURRENT_ROLE: { reserved: true },
+  CURRENT_SCHEMA: { reserved: true, canBeFunctionOrType: true },
+  CURRENT_TIME: { reserved: true },
+  CURRENT_TIMESTAMP: { reserved: true },
+  CURRENT_USER: { reserved: true },
+  DAY: { requiresAs: true },
+  DEC: { cannotBeFunctionOrType: true },
+  DECIMAL: { cannotBeFunctionOrType: true },
+  DEFAULT: { reserved: true },
+  DEFERRABLE: { reserved: true },
+  DESC: { reserved: true },
+  DISTINCT: { reserved: true },
+  DO: { reserved: true },
+  ELSE: { reserved: true },
+  END: { reserved: true },
+  EXCEPT: { reserved: true, requiresAs: true },
+  EXISTS: { cannotBeFunctionOrType: true },
+  EXTRACT: { cannotBeFunctionOrType: true },
+  FALSE: { reserved: true },
+  FETCH: { reserved: true, requiresAs: true },
+  FILTER: { requiresAs: true },
+  FLOAT: { cannotBeFunctionOrType: true },
+  FOR: { reserved: true, requiresAs: true },
+  FOREIGN: { reserved: true },
+  FREEZE: { reserved: true, canBeFunctionOrType: true },
+  FROM: { reserved: true, requiresAs: true },
+  FULL: { reserved: true, canBeFunctionOrType: true },
+  GRANT: { reserved: true, requiresAs: true },
+  GREATEST: { cannotBeFunctionOrType: true },
+  GROUP: { reserved: true, requiresAs: true },
+  GROUPING: { cannotBeFunctionOrType: true },
+  HAVING: { reserved: true, requiresAs: true },
+  HOUR: { requiresAs: true },
+  ILIKE: { reserved: true, canBeFunctionOrType: true },
+  IN: { reserved: true },
+  INITIALLY: { reserved: true },
+  INNER: { reserved: true, canBeFunctionOrType: true },
+  INOUT: { cannotBeFunctionOrType: true },
+  INT: { cannotBeFunctionOrType: true },
+  INTEGER: { cannotBeFunctionOrType: true },
+  INTERSECT: { reserved: true, requiresAs: true },
+  INTERVAL: { cannotBeFunctionOrType: true },
+  INTO: { reserved: true, requiresAs: true },
+  IS: { reserved: true, canBeFunctionOrType: true },
+  ISNULL: { reserved: true, canBeFunctionOrType: true, requiresAs: true },
+  JOIN: { reserved: true, canBeFunctionOrType: true },
+  LATERAL: { reserved: true },
+  LEADING: { reserved: true },
+  LEAST: { cannotBeFunctionOrType: true },
+  LEFT: { reserved: true, canBeFunctionOrType: true },
+  LIKE: { reserved: true, canBeFunctionOrType: true },
+  LIMIT: { reserved: true, requiresAs: true },
+  LOCALTIME: { reserved: true },
+  LOCALTIMESTAMP: { reserved: true },
+  MINUTE: { requiresAs: true },
+  MONTH: { requiresAs: true },
+  NATIONAL: { cannotBeFunctionOrType: true },
+  NATURAL: { reserved: true, canBeFunctionOrType: true },
+  NCHAR: { cannotBeFunctionOrType: true },
+  NONE: { cannotBeFunctionOrType: true },
+  NORMALIZE: { cannotBeFunctionOrType: true },
+  NOT: { reserved: true },
+  NOTNULL: { reserved: true, canBeFunctionOrType: true, requiresAs: true },
+  NULL: { reserved: true },
+  NULLIF: { cannotBeFunctionOrType: true },
+  NUMERIC: { cannotBeFunctionOrType: true },
+  OFFSET: { reserved: true, requiresAs: true },
+  ON: { reserved: true, requiresAs: true },
+  ONLY: { reserved: true },
+  OR: { reserved: true },
+  ORDER: { reserved: true, requiresAs: true },
+  OUT: { cannotBeFunctionOrType: true },
+  OUTER: { reserved: true, canBeFunctionOrType: true },
+  OVER: { requiresAs: true },
+  OVERLAPS: { reserved: true, canBeFunctionOrType: true, requiresAs: true },
+  OVERLAY: { cannotBeFunctionOrType: true },
+  PLACING: { reserved: true },
+  POSITION: { cannotBeFunctionOrType: true },
+  PRECISION: { cannotBeFunctionOrType: true, requiresAs: true },
+  PRIMARY: { reserved: true },
+  REAL: { cannotBeFunctionOrType: true },
+  REFERENCES: { reserved: true },
+  RETURNING: { reserved: true, requiresAs: true },
+  RIGHT: { reserved: true, canBeFunctionOrType: true },
+  ROW: { cannotBeFunctionOrType: true },
+  SECOND: { requiresAs: true },
+  SELECT: { reserved: true },
+  SESSION_USER: { reserved: true },
+  SETOF: { cannotBeFunctionOrType: true },
+  SIMILAR: { reserved: true, canBeFunctionOrType: true },
+  SMALLINT: { cannotBeFunctionOrType: true },
+  SOME: { reserved: true },
+  SUBSTRING: { cannotBeFunctionOrType: true },
+  SYMMETRIC: { reserved: true },
+  SYSTEM_USER: { reserved: true },
+  TABLE: { reserved: true },
+  TABLESAMPLE: { reserved: true, canBeFunctionOrType: true },
+  THEN: { reserved: true },
+  TIME: { cannotBeFunctionOrType: true },
+  TIMESTAMP: { cannotBeFunctionOrType: true },
+  TO: { reserved: true, requiresAs: true },
+  TRAILING: { reserved: true },
+  TREAT: { cannotBeFunctionOrType: true },
+  TRIM: { cannotBeFunctionOrType: true },
+  TRUE: { reserved: true },
+  UNION: { reserved: true, requiresAs: true },
+  UNIQUE: { reserved: true },
+  USER: { reserved: true },
+  USING: { reserved: true },
+  VALUES: { cannotBeFunctionOrType: true },
+  VARCHAR: { cannotBeFunctionOrType: true },
+  VARIADIC: { reserved: true },
+  VARYING: { requiresAs: true },
+  VERBOSE: { reserved: true, canBeFunctionOrType: true },
+  WHEN: { reserved: true },
+  WHERE: { reserved: true, requiresAs: true },
+  WINDOW: { reserved: true, requiresAs: true },
+  WITH: { reserved: true, requiresAs: true },
+  WITHIN: { requiresAs: true },
+  WITHOUT: { requiresAs: true },
+  XMLATTRIBUTES: { cannotBeFunctionOrType: true },
+  XMLCONCAT: { cannotBeFunctionOrType: true },
+  XMLELEMENT: { cannotBeFunctionOrType: true },
+  XMLEXISTS: { cannotBeFunctionOrType: true },
+  XMLFOREST: { cannotBeFunctionOrType: true },
+  XMLNAMESPACES: { cannotBeFunctionOrType: true },
+  XMLPARSE: { cannotBeFunctionOrType: true },
+  XMLPI: { cannotBeFunctionOrType: true },
+  XMLROOT: { cannotBeFunctionOrType: true },
+  XMLSERIALIZE: { cannotBeFunctionOrType: true },
+  XMLTABLE: { cannotBeFunctionOrType: true },
+  YEAR: { requiresAs: true },
+};
+
+export const postgresqlKeywords = {
+  ...Object.fromEntries(
+    Object.entries(keywordDefs)
+      .filter(([_, def]) => def.reserved || def.requiresAs)
+      .map(([kw]) => [kw, true])
+  ),
   // TODO: Temporarily including these actually non-reserved words
   SET: true,
 };
