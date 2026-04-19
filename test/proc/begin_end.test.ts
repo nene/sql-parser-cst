cde7b729a94a01f6c68ea4f67452afc225ad6fc2
Rene Saarsoo <nene@triin.net>
2026-04-19 12:29:43 +0300

    Ensure empty BEGIN...END is supported by PL/pgSQL

--- test/proc/begin_end.test.ts
+++ test/proc/begin_end.test.ts
@@ -191,7 +191,7 @@ describe("BEGIN..END", () => {
     });
   });
 
-  dialect("bigquery", () => {
+  dialect(["bigquery", "plpgsql"], () => {
     it("supports empty BEGIN..END", () => {
       testWc(`BEGIN END;`);
     });
